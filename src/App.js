607533d80df818331bf73e662c544ecebfe09c7d
Felicity-m <70552410+Felicity-m@users.noreply.github.com>
2021-09-27 09:34:49 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+
+import "./app.css"
+import Home from "./components/pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./components/pages/userList/UserList";
+import User from "./components/pages/user/User";
+import NewUser from "./components/pages/newUser/NewUser";
+import ProductList from "./components/pages/productList/ProductList";
+import Product from "./components/pages/product/Product";
+import NewProduct from "./components/pages/NewProduct/NewProduct";
+
+
+
+const App=() =>{
+  return (
+    <Router> 
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/users">
+            <UserList />
+          </Route>
+          <Route exact path="/user/:userId">
+            <User />
+          </Route>
+          <Route exact path="/newUser">
+            <NewUser />
+          </Route>
+          <Route exact path="/products">
+            <ProductList />
+          </Route>
+          <Route exact path="/product/:productId">
+            <Product />
+          </Route>
+          <Route exact path="/newProduct">
+            <NewProduct />
+          </Route>
+
+         
+        </Switch>
+        
+      </div>
+    </div>
+    </Router>
+
+     
+  );
+}
+
+export default App;
